e51511ec31800f13eb454d3774669f61d92d960b
Larry Lade <lplade@users.noreply.github.com>
2017-04-15 23:39:45 -0500

    Installed Bootstrap, set up basic HTML template

--- gutencloud/__init__.py
+++ gutencloud/__init__.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+import logging
 
+# Sets the verbosity of console logging
+logging.basicConfig(level=logging.DEBUG)
+
+# Any needed Flask configuration can be passed as arguments to this
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+#########
+# Routes
+#########
 
+@app.route('/', methods=["GET"])
+def index():
+    return render_template("main.html")
+
+
+# Leave this as the last route
+@app.route("/<path:path>")
+def serve_static(path):
+    """
+    Assume any request not matching above routes is request for static resource
+    :param path:
+    :return:
+    """
+    return app.send_static_file(path)
+
+##################
+# Run application
+##################
 
 if __name__ == '__main__':
     app.run()
