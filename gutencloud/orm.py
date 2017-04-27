021baaf8d89c53161f2120455a293eb5c4d1d615
Larry Lade <lplade@users.noreply.github.com>
2017-04-26 21:28:09 -0500

    Updated the model

--- gutencloud/orm.py
+++ gutencloud/orm.py
@@ -1,5 +1,8 @@
 import os
 
+import logging
+import pprint
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import datetime
@@ -13,9 +16,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATATBASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Squelch warnings
 db = SQLAlchemy(app)
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+MIME_UTF8 = 'text/plain'
+MIME_ASCII = 'text/plain: charset=us-ascii'
 
 class Ebook(db.Model):
-    '''
+    """
+The dict we pass into this looks like:
     {'LCC': {'PS'},
  'author': u'Burroughs, Edgar Rice',
  'authoryearofbirth': 1875,
@@ -36,26 +45,35 @@ class Ebook(db.Model):
   'Science fiction'},
  'title': u"At the Earth's Core",
  'type': 'Text'}
-    '''
+    """
 
     # TODO Really, author, formats, languages, and subjects
     # should be their own tables/objects
 
-    ebook_id = db.Column(db.Integer, primary_key=True)
+    ebook_id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(256), nullable=False)
     author = db.Column(db.String(256))
     author_year_of_birth = db.Column(db.Integer)
     author_year_of_death = db.Column(db.Integer)
     downloads = db.Column(db.Integer)
     type = db.Column(db.String(64))  # 'Text'
+
     # Formats is a dict
-    formats = db.Column(db.PickleType)
+    # We only need the text/plain key
+    plaintext_url = db.Column(db.String(512))
+    plaintext_filename = db.Column(db.String(256))
+
     # language is a list of ISO codes
-    languages = db.Column(db.PickleType)
+    # We are already filtering for 'en'
+    # languages = db.Column(db.PickleType)
+
     # subjects is a set of strings?
-    subjects = db.Column(db.PickleType)
+    # We aren't using this
+    # subjects = db.Column(db.PickleType)
+
     # lcc is a list of two-letter codes (set?)
-    lcc = db.Column(db.PickleType)
+    # We aren't using this either
+    # lcc = db.Column(db.PickleType)
 
     def __init__(self, id, title, *,
                  # First two are required to construct
@@ -64,19 +82,37 @@ class Ebook(db.Model):
                  authoryearofbirth=None,
                  authoryearofdeath=None,
                  downloads=None, formats=None,
-                 language=None, subjects=None,
-                 type=None, LCC=None):
+                 # language=None,
+                 # subjects=None,
+                 type=None     # Let's test for 'Text' before we create object and not store this
+                 # LCC=None
+                 ):
         self.ebook_id = id
         self.title = title
         self.author = author
         self.author_year_of_birth = authoryearofbirth
         self.author_year_of_death = authoryearofdeath
         self.downloads = downloads
-        self.formats = formats
-        self.languages = language
-        self.subjects = subjects
-        self.type = type
-        self.lcc = LCC
+        # self.languages = language
+        # self.subjects = subjects
+        # self.type = type
+        assert type == 'Text'
+        # self.lcc = LCC
+
+        # Prefer to get UTF-8 version, fall back to ASCII if needed
+        if MIME_UTF8 in formats:
+            self.plaintext_url = formats[MIME_UTF8]
+            self.plaintext_filename = self.plaintext_url.rsplit('/', 1)[-1]
+        elif MIME_ASCII in formats:
+            self.plaintext_url = formats[MIME_ASCII]
+            self.plaintext_filename = self.plaintext_url.rsplit('/', 1)[-1]
+        else:
+            logger.warning("Can't find a plaintext version for ebook %d %s", self.ebook_id, self.title)
+            logger.warning("Available formats:")
+            logger.warning(pprint.pformat(formats))
+            self.plaintext_url = None
+            self.plaintext_filename = None
+            # TODO throw error or something?
 
     def __repr__(self):
         return '{}: {} by {} ({}-{})'.format(
@@ -86,3 +122,5 @@ class Ebook(db.Model):
             self.author_year_of_birth,
             self.author_year_of_death
         )
+
+
--- utilities/populate_db.py
+++ utilities/populate_db.py
@@ -1,4 +1,7 @@
 import gc
+
+import logging
+
 from utilities import gutenberg
 from utilities import url_harvester
 from flask import Flask
@@ -6,28 +9,43 @@ from flask_sqlalchemy import SQLAlchemy
 from gutencloud.orm import Ebook
 from os import path
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     path.join('..', 'local_data', 'gutenberg.db')
 db = SQLAlchemy(app)
 
+# Completely re-initialize the database
+db.drop_all()
 db.create_all()
 
-
 # This will take a while
-md = gutenberg.read_metadata()
+metadata_catalog = gutenberg.read_metadata()
 
-# We get back a list of JSON elements
+# We get back a list of dicts
 
 # Store that in the DB for now
 
-for _ebook in md:
-    # This constructs a new Ebook passing the dict as kwargs
-    # Python!
-    new_ebook = Ebook(**_ebook)
+for _ebook in metadata_catalog:
 
+    if _ebook['type'] == 'Text':
+        # This constructs a new Ebook passing the dict as kwargs
+        # Python!
+        new_ebook = Ebook(**_ebook)
+
+        db.session.add(new_ebook)
+        db.session.commit()
+        logger.debug('Added ebook %d to database', _ebook['id'])
+    else:
+        logger.debug('Ebook %d is of type %s, not adding', _ebook['id'], _ebook['type'])
 
 # Get that ginormous list out of memory before proceeding
-del md
+del metadata_catalog
 gc.collect()
 
+# This will also take a while
+link_dict = url_harvester.spider()
+
+for _link in link_dict:
